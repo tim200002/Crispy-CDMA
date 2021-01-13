2a63d29cd4247c96dc28195440d448bf14b61a16
st161286 <st161286@stud.uni-stuttgart.de>
2021-01-13 17:08:15 +0100

    Update test.m

--- test.m
+++ test.m
@@ -1,27 +1,85 @@
-codeLength =16;
+clear
+
+codeLength =2;
 cdmaEncoder = CDMAEncoder(codeLength);
-testSignal1 = Signal([1,0,1,1,0,0,0,0],10);
+signal_length = 1000;
+testSignal1 = Signal( round(0.75*rand(1,signal_length)),1e3); %Wie muss die Abtastfrequenz gesetzt werden
+disp('Original')
+testSignal1.data(1:10)
+
 cdmaSignal1 = cdmaEncoder.step(testSignal1,2);
 
-testSignal2 = Signal([0,0,1,1,0,0,0,0],10);
+
+testSignal2 = Signal( round(0.75*rand(1,signal_length)),1e3);
 cdmaSignal2 = cdmaEncoder.step(testSignal2,1);
 
-testSignal3 = Signal([0,0,1,1,1,0,1,0],10);
-cdmaSignal3 = cdmaEncoder.step(testSignal3,5);
 
 
-addedSignal =cdmaSignal1+cdmaSignal2+cdmaSignal3;
-
-
+addedSignal =cdmaSignal1+cdmaSignal2;
 
 pamMapper = PAMMapper(codeLength);
-channelSignal = pamMapper.step(addedSignal);
+afterMapper = pamMapper.step(addedSignal);
 
-ch = Channel('AWGN',10);
-afterChannel=ch.step(channelSignal)
+%Pulse shaped
+pulseShaper = Pulseshaper(Impulsetype.RaisedCosine, 16);
+afterPulseShaper = pulseShaper.step(afterMapper);
+
+
+
+
+%Modulate Signal
+scope = Scope();
+fc = 13e3;
+mixer = Mixer(Mixertype.Cosine, fc);
+afterMixer = mixer.step(afterPulseShaper);
+
+%Insert Pilot
+pilotInserter = PilotInserter(fc);
+pilotedSignal = pilotInserter.step(afterMixer);
+
+
+
+
+
+%Search for pilot
+synchronizer = Synchronizer(fc);
+pilotIndex = synchronizer.step(pilotedSignal);
+
+
+
+removedPilot = Signal(pilotedSignal.data(pilotIndex:end), pilotedSignal.fs);
+
+%Demodulate Signal
+afterDemodulation = mixer.step(removedPilot);
+%Filter Signal
+load('untitled.mat');
+filter = Filter(32e3, Num);
+afterFilt = filter.step(afterDemodulation);
+figure(1)
+plot(afterFilt.data(1:1000)*2)
+figure(2)
+plot(afterPulseShaper.data(1:1000))
+
+%Read Bits
+bitIndex = [1: 16: afterFilt.length];
+unformed = Signal(afterFilt.data(bitIndex)*2, afterDemodulation.fs/16);
+
+
+
+
+
+
+% figure(2)
+% plot(afterDemodulation.data(1:1000));
+
+
+
+
 
 pamDemapper = PAMDemapper(codeLength);
-demappedSignal = pamDemapper.step(afterChannel);
+demappedSignal = pamDemapper.step(unformed);
 
 cdmaDecoder = CDMADecoder(codeLength);
-cdmaDecoder.step(demappedSignal,2)
+resSignal = cdmaDecoder.step(demappedSignal,2);
+disp('Result')
+resSignal.data(1:10)

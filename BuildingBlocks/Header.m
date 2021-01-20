c9617bfdef966371e94d29d2d3d781d2834a184a
lindenau <lindenau@fp>
2021-01-20 21:56:54 +0100

    First Version to send on channel. Walks almost only last few bits off. Don't know why

--- BuildingBlocks/Header.m
+++ BuildingBlocks/Header.m
@@ -1,6 +1,7 @@
 classdef Header
     properties
         lengthInBits
+        onesLength = 4
     end
     
     methods
@@ -25,19 +26,19 @@ classdef Header
            
 
             %Header Always starts with 4 ones in a row
-            one = [1;1;1;1];
+            one = ones(1,obj.onesLength).';
             bitVector = [one; bitVector];
             retSignal =Signal([bitVector; signal.data], signal.fs);   
         end
         
         function [retSignal, retLength] = removeHeaderAndGetLength(obj, signal)
            %Create Scale Factor from ones at front
-            ones = signal.data(1: 4);
-            scaleFaktor = 1/mean(ones);
-            header =scaleFaktor* signal.data(5:obj.lengthInBits + 4); %4 because 4 ones added in front
+            ones = signal.data(1: obj.onesLength)
+            scaleFaktor = 1/mean(ones)
+            header =scaleFaktor * signal.data(obj.onesLength+1:obj.lengthInBits + obj.onesLength) %4 because 4 ones added in front
             
                         
-            retSignal = Signal(signal.data(obj.lengthInBits+4+1:end)*scaleFaktor, signal.fs);
+            retSignal = Signal(signal.data(obj.lengthInBits+obj.onesLength+1:end)*scaleFaktor, signal.fs);
             
 
             
--- transmitter.m
+++ transmitter.m
@@ -7,11 +7,11 @@ fc = 13e3;
 samplesPerSymbol=15;
 symbolRate = 0.8e3;
 
-audioDeviceId=1;
+audioDeviceId=6;
 
  
  %% Serialize Image
-serializer = ImageSerializer('TestImages\tvTestScreen32x32.jpg');
+serializer = ImageSerializer('TestImages/tvTestScreen32x32.jpg');
 bitStream=serializer.GenerateRGBBitStream();
 cdmaEncoder = CDMAEncoder(codeLength);
 signal_length = 1000;
@@ -28,12 +28,12 @@ cdmaSignal3 = cdmaEncoder.step(bitSignal3,3);
 addedSignal =cdmaSignal1+cdmaSignal2+cdmaSignal3;
 
 pamMapper = PAMMapper(codeLength);
-afterMapper = pamMapper.step(addedSignal);
+afterMapper = pamMapper.step(addedSignal)
 
 %% Add Header
 header = Header(headerLength);
 
-headerSignal = header.addHeader(afterMapper);
+headerSignal = header.addHeader(afterMapper)
 
 %% Modulation
  pilotInserter = PilotInserter(fc);
@@ -42,9 +42,11 @@ headerSignal = header.addHeader(afterMapper);
  
  pulseShapedSignal = pulseShaper.step(headerSignal);
  mixedSignal = mixer.step(pulseShapedSignal);
+ figure(4)
+ plot(mixedSignal.data)
  pilotedSignal = pilotInserter.step(mixedSignal);
- modulatedSignal = pilotedSignal;
+ modulatedSignal = Signal(pilotedSignal.data, pilotedSignal.fs)
 
  %% Transmit Signal
  sink = Sink(audioDeviceId, 48e3, '16-bit integer');
- sink.step(modulatedSignal);
+ sink.step(modulatedSignal)
